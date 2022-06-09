cce6d9017b4263e473d584b713c69e024086fb2d
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-06-09 13:48:55 +0430

    Create routes & import components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import ProductListing from "./components/ProductListing";
+import ProductDetails from "./components/ProductDetails";
 
 function App() {
-  return <div className="App">hiii</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<ProductListing />}></Route>
+        <Route path="/product/:productId" element={<ProductDetails />}></Route>
+        <Route>404 Not Found!</Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

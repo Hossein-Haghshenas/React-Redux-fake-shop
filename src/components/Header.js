646b3ed254344de9026a50f9774168110a7ee601
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-06-09 13:44:33 +0430

    Create header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="ui fixed menu">
+      <section className="ui container center">
+        <h2>USA Shop</h2>
+      </section>
+    </header>
+  );
+};
+
+export default Header;

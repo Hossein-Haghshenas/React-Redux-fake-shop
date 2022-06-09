13196650a33d748e047529ae0e0341eed1c984b7
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-06-09 10:56:22 +0430

    Create actions

---
+++ src/redux/actions/productAction.js
@@ -0,0 +1,17 @@
+import { ActionTypes } from "../contants/action-types";
+
+const { SET_PRODUCTS, SELECTED_PRODUCT } = ActionTypes;
+
+export const setProducts = (products) => {
+  return {
+    type: SET_PRODUCTS,
+    payload: products,
+  };
+};
+
+export const selectedProduct = (product) => {
+  return {
+    type: SELECTED_PRODUCT,
+    payload: product,
+  };
+};
